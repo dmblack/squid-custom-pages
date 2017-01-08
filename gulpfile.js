16c17403a29b0cfdd3a651dfc72b53928484d8ca
dmblack <dustyn@remexchange.net>
2017-01-08 16:47:01 +1100

    Futher work on custom pages, and gulp files. Gulp file now injects style into page, rather than as included link.

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,15 @@ gulp.task('styles', function(){
 })
 
 gulp.task('html', ['styles'], function(){
-  var injectFiles = gulp.src(['distribution/pages/css/main.css'])
-
   return gulp.src('src/pages/ERR_ACCESS_DENIED')
-    .pipe(inject(injectFiles))
+
+  .pipe(inject(gulp.src(['distribution/pages/css/main.css']), {
+    starttag: '<!-- inject:css -->',
+    relative: true,
+    transform: function (filePath, file) {
+      // return file contents as string
+      return file.contents.toString('utf8')
+    }
+  }))
     .pipe(gulp.dest('distribution/pages/'))
 })
